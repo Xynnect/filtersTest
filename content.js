e57f69cf157ae0611d327f3cc8144abecd1b3773
Xelnect <dido.jordanov7@gmail.com>
2015-07-29 18:59:21 +0300

    Initial commit

---
+++ content.js
@@ -0,0 +1,5 @@
+/**
+ * Created by XelnectPC on 7/29/2015.
+ */
+//document.body.innerHTML = document.body.innerHTML.replace("comments", "opinions");
+document.body.innerHTML = document.body.innerHTML.replace(new RegExp("comments", "g"), "nobody");
